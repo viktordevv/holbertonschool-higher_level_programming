79de356347b26bd454107a38983fe7484df5c918
Viktor <4640@holbertonschool.com>
2022-05-02 15:03:35 -0700

    execute

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,3 +3,4 @@ str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 stri = str [39:66] + " with Python"
 print(stri)
+
