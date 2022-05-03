f265e211993dc69806f638b48854335452c9de92
Viktor <viktorkrilldj@gmail.com>
2022-05-03 12:50:03 -0500

    last digit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,9 @@
-if 12 == 48/4:
-    print("Holberton")
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number < 0:
+    print(f"{number} is negative")
+elif number == 0:
+    print(f"{number} is zero")
 else:
-    print("School")
+    print(f"{number} is positive")
