d8f698f3acddc53bf507a5e372e5708e5c45a2dc
Viktor <viktorkrilldj@gmail.com>
2022-05-03 13:32:49 -0500

    exce

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for c in range(97, 101):
+        print(chr(c), end='')
+for d in range(102, 113):
+        print(chr(d), end='')
+for e in range(114, 123):
+        print(chr(e), end='')
