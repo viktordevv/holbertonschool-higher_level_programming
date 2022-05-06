75df48ebb04c250e6e06e9ffe5fb08e75dd69095
Viktor <viktorkrilldj@gmail.com>
2022-05-05 19:51:58 -0500

    infinity war

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    if (len(argv) == 1):
-        print("0 arguments.")
-    elif (len(argv) == 2):
-        print("1 argument:")
-        print(f"1: {argv[1]}")
-    else:
-        print(f"{len(argv) - 1} arguments:")
-        for i, j in enumerate(argv):
-            if (i != 0):
-                print(f"{i}: {j}")
+import sys
+
+if __name__ == '__main__':
+    rem = sys.argv
+    o_rem = len(rem)
+    sum = 0
+
+    if o_rem > 1:
+        for i in range(1, o_rem):
+            sum += int(rem[i])
+
+    print(sum)
