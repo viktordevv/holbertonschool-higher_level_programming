e44a673b054c1acedcf252e1e10576bf6280fedb
Viktor <viktorkrilldj@gmail.com>
2022-05-09 21:27:58 -0500

    nux

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    else:
-        return max(my_list)
+
+    nux = min(my_list)
+    for i in my_list:
+        if i > nux:
+            nux = i
+
+    return nux
