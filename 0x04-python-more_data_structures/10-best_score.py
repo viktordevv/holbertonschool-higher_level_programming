37a23731adda7599a178c9d23216b150b7c76036
Viktor <viktorkrilldj@gmail.com>
2022-05-10 16:09:07 -0500

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     if a_dictionary:
         return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
-    return None
