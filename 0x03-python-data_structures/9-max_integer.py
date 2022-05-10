8c9333c05867de0a7da334ea1c6dcae1a7a0a81e
Viktor <viktorkrilldj@gmail.com>
2022-05-09 21:14:27 -0500

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 1:
-        return None
-    else:
+    if my_list:
         return max(my_list)
+    else:
+        return (None)
