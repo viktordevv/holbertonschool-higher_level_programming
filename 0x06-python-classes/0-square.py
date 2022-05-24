9a828131147a1b1d85772b500ca3a64612edca14
Viktor <viktorkrilldj@gmail.com>
2022-05-24 03:39:53 -0500

    first opp

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Square class
+Esto escribe una funcion vacia """
+
+
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Square class
+Esto escribe una funcion de size """
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
