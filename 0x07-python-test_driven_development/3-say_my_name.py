8f8c53615789807e2c7cae451858737e4789485c
Viktor <viktorkrilldj@gmail.com>
2022-05-31 00:00:56 -0500

    strings

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,3 +3,14 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>
+    Args:   (str) first_name: first string
+            (str) last_name: second string, default str = empty
+    Raises: TypeError: if arguments are not string type """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
