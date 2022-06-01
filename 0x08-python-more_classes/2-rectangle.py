69097377d2cf5923ea8b14f29f7520464809fd89
Viktor <viktorkrilldj@gmail.com>
2022-05-31 21:57:48 -0500

    delete space

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for width"""
-        if type (value) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
