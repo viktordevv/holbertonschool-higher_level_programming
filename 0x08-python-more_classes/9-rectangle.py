ccc9d92635be5c59c65f66e6482f8bd64d86ca12
Viktor <viktorkrilldj@gmail.com>
2022-05-31 23:02:50 -0500

    ZzZzZz

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the rectangle"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
