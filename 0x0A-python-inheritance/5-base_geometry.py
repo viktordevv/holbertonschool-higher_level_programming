18aff26670bb18233fe6eedc1c364bb34d3ae973
Viktor <viktorkrilldj@gmail.com>
2022-06-29 21:02:34 -0500

    base

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the class Base
+"""
+
+
+class BaseGeometry:
+    """Aqui no hay nadita"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains the class base
+"""
+
+
+class BaseGeometry:
+    """Esta es la clase area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+        
