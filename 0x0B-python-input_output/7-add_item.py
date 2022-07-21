0ff3a77863baa5f70d3d75495c456baab44f2e82
Viktor <viktorkrilldj@gmail.com>
2022-07-21 14:19:21 -0500

    to json creates

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list and save"""
+"""script that adds all arguments to a Python list
+and then save them to a file
+"""
 
 
 import sys
