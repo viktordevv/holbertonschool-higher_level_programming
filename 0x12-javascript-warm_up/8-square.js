dd6b36270647e6b52d68ea858670b00880cf5258
Viktor <viktorkrilldj@gmail.com>
2022-08-15 14:59:18 -0500

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (size) {
+  for (let q = 0; q < size; ++q) {
+    let p = 0;
+
+    for (; p < size; ++p) {
+      process.stdout.write('X');
+    }
+
+    if (p === size) {
+      console.log("");
+    }
+  }
+} else {
+  console.log('Missing size');
+}
