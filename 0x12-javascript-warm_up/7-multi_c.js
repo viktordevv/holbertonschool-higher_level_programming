137bcd723925c0308e02828ea631fcee8748fb22
Viktor <viktorkrilldj@gmail.com>
2022-08-15 15:02:08 -0500

    deletes spaces

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const limites = parseInt(process.argv[2]);
 
 if (limites) {
   for (let i = 0; i < limites; ++i) {
-    console.log('C is fun');
-  }
+  console.log('C is fun');
+}
 } else {
     console.log('Missing number of occurrences');
 }
