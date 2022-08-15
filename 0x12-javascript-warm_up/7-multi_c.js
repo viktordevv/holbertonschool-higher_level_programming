48012aefadc17c59257a38bec1e0e0e3ad5b84f7
Viktor <viktorkrilldj@gmail.com>
2022-08-15 15:05:21 -0500

    semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const limites = parseInt(process.argv[2]);
-
 if (limites) {
   for (let i = 0; i < limites; ++i) {
-  console.log('C is fun');
-}
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
