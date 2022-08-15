f361e9189679bce610d69ac7012efb1500a33ee5
Viktor <viktorkrilldj@gmail.com>
2022-08-15 14:53:53 -0500

    limites

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const limites = parseInt(process.argv[2]);
+
+if (limites) {
+  for (let i = 0; i < limites; ++i) {
+    console.log('C is fun');
+  }
+} else {
+    console.log("Missing number of occurrences");
+}
