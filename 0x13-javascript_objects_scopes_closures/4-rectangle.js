22019f62897bc7fc2cbdbdbd9a15fa7359614d4a
Viktor <viktorkrilldj@gmail.com>
2022-08-17 22:20:24 -0500

    parentheses

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,14 +2,14 @@
 // Rotate and Double the width and height of the Rectangle
 
 module.exports = class Rectangle {
-  constructor(width, height) {
+  constructor (width, height) {
     if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
       this.width = width;
       this.height = height;
     }
   }
 
-  print(char = 'X') {
+  print (char = 'X') {
     for (let i = 0; i < this.height; ++i) {
       let j = 0;
 
@@ -23,11 +23,11 @@ module.exports = class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
