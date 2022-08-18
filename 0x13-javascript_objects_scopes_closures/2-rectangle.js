6ff02a3166d319964daeaf4832596f3a8b4a6c78
Viktor <viktorkrilldj@gmail.com>
2022-08-17 21:51:00 -0500

    excuse

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // Rectangle parameters
 
 module.exports = class Rectangle {
-  constructor(width, height) {
+  constructor (width, height) {
     if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
       this.width = width;
       this.height = height;
