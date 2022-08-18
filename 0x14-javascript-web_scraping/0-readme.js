b56edce4c356ec9383a0cbc2aa75594d9978689e
Viktor <viktorkrilldj@gmail.com>
2022-08-17 23:38:01 -0500

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
 
+const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(file, 'utf-8', function (err, data) {
+  console.log(err || data);
 });
