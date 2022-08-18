36fd3327a0c2a215eec9d042a9a7b8fa4c691d5f
Viktor <viktorkrilldj@gmail.com>
2022-08-17 22:19:19 -0500

    spaces22

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,33 +2,33 @@
 // Rotate and Double the width and height of the Rectangle
 
 module.exports = class Rectangle {
-	constructor (width, height) {
-		if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-			this.width = width;
-			this.height = height;
-		}
-	}
+  constructor(width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
 
-	print (char = 'X') {
-		for (let i = 0; i < this.height; ++i) {
-			let j = 0;
+  print(char = 'X') {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
 
-			for (; j < this.width; ++j) {
-				process.stdout.write(char);
-			}
+      for (; j < this.width; ++j) {
+        process.stdout.write(char);
+      }
 
-			if (j === this.width) {
-				console.log('');
-			}
-		}
-	}
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
 
-	rotate () {
-		[this.width, this.height] = [this.height, this.width];
-	}
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-	double () {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
