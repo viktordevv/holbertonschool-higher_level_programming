2e5c4b8a500075f0197bb8763b406ca3ed585818
Viktor <viktorkrilldj@gmail.com>
2022-08-30 17:04:43 -0500

    create files

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -0,0 +1,9 @@
+-- Create the database hbtn_0e_100_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
+USE hbtn_0e_100_usa;
+
+SELECT * FROM states;
+SELECT * FROM cities;
+
+Enter password: 
+ERROR 1146 (42S02) at line 5: Table 'hbtn_0e_100_usa.states' doesn't exist
---
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -0,0 +1,7 @@
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW CREATE TABLE states;
+
+Enter password: 
+ERROR 1146 (42S02) at line 4: Table 'hbtn_0e_6_usa.states' doesn't exist
