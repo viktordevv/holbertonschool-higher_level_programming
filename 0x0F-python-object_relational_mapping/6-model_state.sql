e53f24a48feff545028e16bde7f207363d6890db
Viktor <viktorkrilldj@gmail.com>
2022-09-08 10:02:28 -0500

    task 6

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -2,6 +2,3 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
 SHOW CREATE TABLE states;
-
-Enter password: 
-ERROR 1146 (42S02) at line 4: Table 'hbtn_0e_6_usa.states' doesn't exist
